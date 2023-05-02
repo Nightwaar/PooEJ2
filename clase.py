87fc2d5aa502aaecf2847a09feeb1c929174537e
MarcosMolina <90269496+Nightwaar@users.noreply.github.com>
2023-05-02 00:03:20 -0300

    Add files via upload

--- clase.py
+++ clase.py
@@ -3,22 +3,26 @@ class Viajero:
     __DNI= ''
     __nombre = ''
     __apellido= ''
-    __millasacumuladas=''
-    def __init__(self,NumeroViajero='',DNI='',nombre='',apellido='',millasacumuladas=''):
+    __millasacumuladas=0
+    def __init__(self,NumeroViajero='',DNI='',nombre='',apellido='',millasacumuladas=0):
         self.__NumeroViajero = NumeroViajero
         self.__DNI = DNI
         self.__nombre = nombre 
         self.__apellido = apellido 
-        self.__millasacumuladas = millasacumuladas 
-    def cantidadTotaldeMillas(self):
-        return 'La cantidad total de millas es: {}'.format(self.__millascumuladas)
-    def acumularmillas(self,millas):
+        self.__millasacumuladas = int(millasacumuladas) 
+    def accion1(self):
+        return self.__millasacumuladas
+    def accion2(self,millas=0):
         self.__millasacumuladas = self.__millasacumuladas + millas
-    def canjearmillas(self,millascanjear):
+        print('La cantidad es {}'.format(self.__millasacumuladas))
+        return 
+    def accion3(self,millascanjear):
         if millascanjear <= self.__millasacumuladas:
+            self.__millasacumuladas = self.__millasacumuladas - millascanjear
             print('Millas canjeadas')
         else: 
-            return 'Error'
+            print('No se puede canjear')
+        return
     def mostrar(self):
             print('El numero de viajero es:{}'.format(self.__NumeroViajero))
             print('El DNI es:{}'.format(self.__DNI))
@@ -28,19 +32,9 @@ class Viajero:
     def verifnum(self,numero):
         if self.__NumeroViajero == numero:
             return True
-    def ingreso(lista,numviajero):
+    def ingreso(lista):
+        numviajero = input("Ingrese un numero de viajero:")
         for i in range(len(lista)):
             if lista[i].verifnum(numviajero) == True:
                 print(lista[i])
-    def op1(self):
-        print('La cantidad de millas es: {}'.format(self.__millasacumuladas))
-    def op2(self):
-        millas = input('Ingrese la cantidad de millas a acumular')
-        self.__millasacumuladas = self.__millasacumuladas + millas
-    def op3(self):
-        millas = input('Ingrese la cantidad de millas a canjear')
-        if self.__millasacumuladas >= millas:
-            self.__millasacumuladas = self.__millasacumuladas - millas
-        else:
-            print('Error')
-        return
+                return i
--- main.py
+++ main.py
@@ -10,5 +10,21 @@ if __name__ == '__main__' :
         listaviajeros.append(Viajero(fila[0],fila[1],fila[2],fila[3],fila[4]))
     for i in range(len(listaviajeros)):
         listaviajeros[i].mostrar()
-    numviaj = input('Ingrese su numero de viajero:')
-    Viajero.ingreso(listaviajeros,numviaj)
+    numviaj=Viajero.ingreso(listaviajeros)
+    print ("1. Consultar millas ")
+    print ("2. Acumular ")
+    print ("3. Canjear ")
+    print ("4. Salir ")
+    opc = int(input("Ingrese opcion :")) 
+    while opc !=4:
+        if opc == 1:
+            print("La cantidad de millas es: {}".format(listaviajeros[numviaj].accion1()))
+        elif opc == 2:
+            millas= int(input("Ingrese cantidad de millas a acumular")) 
+            listaviajeros[numviaj].accion2(millas)
+        elif opc == 3:
+            millasACanjear = int(input("Ingrese la cantidad de millas a canjear "))
+            listaviajeros[numviaj].accion3(millasACanjear)
+        elif opc == 4:
+            print ("\n Gracias por ingresar ")
+        opc = int(input("2Ingrese opcion "))
